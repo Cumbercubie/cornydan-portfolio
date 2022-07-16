4231178d7fcdbc9cf02e4be930382b409dda934e
thuocsi.huancao <huan.cao@thuocsi.nv>
2022-07-16 11:09:43 +0700

    update build

---
+++ build/precache-manifest.0f3c27735b032f702d441f5dfd9c6a2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fda5b1c753bedca02d43",
+    "url": "/cornydan-portfolio/static/css/main.e5216ce7.chunk.css"
+  },
+  {
+    "revision": "fda5b1c753bedca02d43",
+    "url": "/cornydan-portfolio/static/js/main.fda5b1c7.chunk.js"
+  },
+  {
+    "revision": "0e03b74260aecbc98837",
+    "url": "/cornydan-portfolio/static/css/1.71b0bc07.chunk.css"
+  },
+  {
+    "revision": "0e03b74260aecbc98837",
+    "url": "/cornydan-portfolio/static/js/1.0e03b742.chunk.js"
+  },
+  {
+    "revision": "b1a8f5f599f6c9b302c0",
+    "url": "/cornydan-portfolio/static/js/runtime~main.b1a8f5f5.js"
+  },
+  {
+    "revision": "e12e74c796f6c6e4f11dc4f92d518442",
+    "url": "/cornydan-portfolio/index.html"
+  }
+];
